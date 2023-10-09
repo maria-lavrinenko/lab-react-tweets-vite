22e64b490275465038292cabc377de0d7751bda8
Maria Lavrinenko <marylavrinenko@gmail.com>
2023-10-09 16:20:04 +0200

    all done

---
+++ src/components/Actions.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Actions() {
+  return (
+    <div className="actions">
+      {/* Font Awesome icons */}
+      <i class="far fa-comment" data-testid="comment-icon"></i>
+      <i class="fas fa-retweet" data-testid="retweet-icon"></i>
+      <i class="far fa-heart" data-testid="heart-icon"></i>
+      <i class="fas fa-share" data-testid="share-icon"></i>
+    </div>
+  );
+}
+
+export default Actions;
---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Timestamp(props) {
+  console.log(props);
+  return <span className="timestamp">{props.time}</span>;
+}
+
+export default Timestamp;
